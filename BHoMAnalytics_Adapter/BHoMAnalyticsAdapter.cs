8422e0842d651c8613a6364eb0646433fdcb8559
adecler <arnaud.declercq@burohappold.com>
2020-07-17 16:01:30 +0800

    Adding the BHoMAnalytics Adapter

---
+++ BHoMAnalytics_Adapter/BHoMAnalyticsAdapter.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+using System.Threading;
+using System.IO;
+using BH.Adapter.Mongo;
+using BH.oM.BHoMAnalytics;
+using System.Collections.Generic;
+
+namespace BH.Adapter.BHoMAnalytics
+{
+    public partial class BHoMAnalyticsAdapter : BHoMAdapter
+    {
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public BHoMAnalyticsAdapter()
+        {
+
+        }
+
+
+        /***************************************************/
+        /**** Static Methods                            ****/
+        /***************************************************/
+
+        static BHoMAnalyticsAdapter()
+        {
+            MongoAdapter mongo = GetTargetDatabase();
+            if (mongo == null)
+                return;
+
+            List<UsageEntry> usageData = Engine.BHoMAnalytics.Compute.CollectUsageData(false);
+            mongo.Push(usageData);
+        }
+
+
+        /***************************************************/
+        /**** Helper Methods                            ****/
+        /***************************************************/
+
+        private static MongoAdapter GetTargetDatabase()
+        {
+            if (m_MongoAdapter == null)
+            {
+                string settingsFile = @"C:\ProgramData\BHoM\Settings\BHoMAnalytics.cfg";
+                if (!File.Exists(settingsFile))
+                    return null;
+
+                try
+                {
+                    string json = File.ReadAllText(settingsFile);
+                    Settings settings = Engine.Serialiser.Convert.FromJson(json) as Settings;
+                    if (settings == null)
+                        return null;
+
+                    m_MongoAdapter = new MongoAdapter(settings.ServerAddress, settings.DatabaseName, settings.CollectionName);
+                }
+                catch { }
+            } 
+
+            return m_MongoAdapter;
+        }
+
+
+        /***************************************************/
+        /**** Private Fields                            ****/
+        /***************************************************/
+
+        private static MongoAdapter m_MongoAdapter = null;
+
+        /***************************************************/
+    }
+}
+
