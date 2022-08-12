4424284476379764640d3b850524008f4d3662de
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2022-08-12 10:33:24 +0100

    Apply suggestions from code review

--- BHoMAnalytics_UI/Properties/AssemblyInfo.cs
+++ BHoMAnalytics_UI/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BHoMAnalytics_UI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/BHoMAnalytics_Toolkit")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BuroHappold Engineering")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BHoMAnalytics_UI")]
-[assembly: AssemblyCopyright("Copyright © BuroHappold Engineering 2022")]
+[assembly: AssemblyCopyright("Copyright © https://github.com/BHoM")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.3.0.0")]
